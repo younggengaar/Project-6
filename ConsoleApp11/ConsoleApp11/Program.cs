8e19b147ccdd33810e60b6d824bd18dc7018fd8f
liza <123323177+younggengaar@users.noreply.github.com>
2023-02-27 23:10:02 +0300

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задание1
+            //int[] omas = new int[5];
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            // Console.Write($"omas[{i}]");
+            // omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //int max = omas[0]; int index = 0;
+            //for (int i = 1; i < omas.Length; i++)
+            //{
+            // if (omas[i] > max)
+            // {
+            // max = omas[i]; index = i;
+            // }
+            //}
+            //Console.WriteLine($"max={max}\t index={index}");
+            //Console.ReadKey();
+
+            //задание2
+            //int[] omas = new int[5];
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            // Console.Write($"omas[{i}]");
+            // omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //int max = omas[0]; int index1 = 0;
+            //int min = omas[0]; int index2 = 0;
+            //for (int i = 1; i < omas.Length; i++)
+            //{
+            // if (omas[i] > max)
+            // {
+            // max = omas[i]; index1 = i;
+            // }
+            // if (omas[i] < min)
+            // {
+            // min = omas[i]; index2 = i;
+            // }
+            //}
+            //int p = omas[index1];
+            //omas[index1] = omas[index2];
+            //omas[index2] = p;
+            //foreach (int el in omas)
+            //{
+            // Console.Write($"{el}\t");
+            //}
+            //Console.ReadKey();
+
+            //задание3
+            //byte[] omas = new byte[5] { 7, 12, 13, 19, 35 };
+            //byte max = omas[0];
+            //byte min = omas[0];
+            //for (byte i = 1; i < omas.Length; i++)
+            //{
+            //    if (omas[i] > max)
+            //    {
+            //        max = omas[i];
+            //    }
+            //    if (omas[i] < min)
+            //    {
+            //        min = omas[i];
+            //    }
+            //}
+            //byte count = 0;
+            //for (byte i = 0; i < omas.Length; i++)
+            //{
+            //    if (omas[i] < max)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine($"max={max}\t min={min}");
+            //Console.WriteLine($"count={count}");
+            //Console.ReadKey();
+
+            //задание4
+            //int[] omas = new int[5] { 2, 3, 6, 8, 10 };
+            //int max = omas[0];
+            //int min = omas[0];
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    if (omas[i] < min)
+            //    {
+            //        min = omas[i];
+            //    }
+            //    if (omas[i] > max)
+            //    {
+            //        max = omas[i];
+            //    }
+            //}
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    omas[i] += max + min;
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //Console.WriteLine($"max={max}\t min={min}");
+            //Console.ReadKey();
+
+        }
+    }
+}
